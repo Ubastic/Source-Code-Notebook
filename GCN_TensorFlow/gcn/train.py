0258d650f2ce98a935adea824e4568c36904ab79
XunKaiLi <lxk_yb@163.com>
2021-02-04 21:34:36 +0800

    GCN_TensorFlow

--- GCN_TensorFlow/gcn/train.py
+++ GCN_TensorFlow/gcn/train.py
@@ -14,6 +14,7 @@ np.random.seed(seed)
 tf.set_random_seed(seed)
 
 # Settings
+# tf 中定义了 tf.app.flags.FLAGS ，用于接受从终端传入的命令行参数
 flags = tf.app.flags
 FLAGS = flags.FLAGS
 flags.DEFINE_string('dataset', 'cora', 'Dataset string.')  # 'cora', 'citeseer', 'pubmed'
@@ -46,7 +47,29 @@ elif FLAGS.model == 'dense':
 else:
     raise ValueError('Invalid argument for model: ' + str(FLAGS.model))
 
+# Tensorflow的设计理念称之为计算流图，在编写程序时，首先构筑整个系统的graph，代码并不会直接生效，
+# 这一点和python的其他数值计算库（如Numpy等）不同，graph为静态的，类似于docker中的镜像。
+# 然后，在实际的运行时，启动一个session，程序才会真正的运行。
+# 这样做的好处就是：避免反复地切换底层程序实际运行的上下文，tensorflow帮你优化整个系统的代码。
+# 我们知道，很多python程序的底层为C语言或者其他语言，执行一行脚本，就要切换一次，是有成本。
+# tensorflow通过计算流图的方式，帮你优化整个session需要执行的代码，还是很有优势的。
+
+# 所以placeholder()函数是在神经网络构建graph的时候在模型中的占位，此时并没有把要输入的数据传入模型，它只会分配必要的内存。
+# 等建立session，在会话中，运行模型的时候通过feed_dict()函数向占位符喂入数据。
 # Define placeholders
+print("type(features):", type(features))    # tuple
+# features[0] 二维数组描述矩阵矩阵存储值的横纵坐标
+# features[1] 存储的值
+# features[2] 维度信息 （2708， 1433）
+print("features[2]:", features[2]) # (2708, 1433)
+# tf.placeholder(
+#     dtype,
+#     shape=None,
+#     name=None
+# )
+# dtype：数据类型。常用的是tf.float32,tf.float64等数值类型
+# shape：数据形状。默认是None，就是一维值，也可以是多维（比如[2,3], [None, 3]表示列是3，行不定）
+# name：名称
 placeholders = {
     'support': [tf.sparse_placeholder(tf.float32) for _ in range(num_supports)],
     'features': tf.sparse_placeholder(tf.float32, shape=tf.constant(features[2], dtype=tf.int64)),
@@ -81,6 +104,7 @@ for epoch in range(FLAGS.epochs):
 
     t = time.time()
     # Construct feed dictionary
+    # 运行模型的时候通过feed_dict()函数向占位符喂入数据
     feed_dict = construct_feed_dict(features, support, y_train, train_mask, placeholders)
     feed_dict.update({placeholders['dropout']: FLAGS.dropout})
 
